ad1fd1929ebe02a99c11d563269eec62a65eac8a
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-08-01 09:46:29 +0200

    Update theory.py
    Updated for imports
    
    Added randn() for sync version

--- asyncio/theory.py
+++ asyncio/theory.py
@@ -1,6 +1,14 @@
+from random import randint
+import time
+import asyncio
+
 def odds(start, stop):
     for odd in range(start, stop+1, 2):
         yield odd
+
+def randn():
+    time.sleep(3)
+    return randint(1, 10)
 
 def main():
     odd_values = [odd for odd in odds(3, 15)]
