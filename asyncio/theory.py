1bb81e852dc38f73e7bf958aa5969c5686a65eef
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-08-01 09:53:04 +0200

    Update theory.py
    Making main() async

--- asyncio/theory.py
+++ asyncio/theory.py
@@ -6,15 +6,17 @@ def odds(start, stop):
     for odd in range(start, stop+1, 2):
         yield odd
 
-def randn():
-    time.sleep(3)
+async def randn():
+    await asyncio.sleep(3)
     return randint(1, 10)
 
-def main():
+async def main():
     odd_values = [odd for odd in odds(3, 15)]
     odds2 = tuple(odds(21, 29))
     print(odd_values)
     print(odds2)
+    print('Randn(): ', await randn())
+
 
 if __name__ == "__main__":
     main()
