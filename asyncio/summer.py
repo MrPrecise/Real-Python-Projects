e390d92f79a572f283fd43664094ec201ae665e2
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-08-02 10:55:51 +0200

    Summer.py
    Created a program that creates a session for a worker which sends a get request which return a JSON object.
    
    Using the key of the JSON Object to extract the data we wanted and sum the number of the data together

---
+++ asyncio/summer.py
@@ -0,0 +1,25 @@
+import asyncio
+import json
+import time
+import aiohttp
+
+
+async def worker(name, n, session):
+    print(f'worker-{name}')
+    url = f'https://qrng.anu.edu.au/API/jsonI.php?length={n}&type=uint16'
+    response = await session.request(method='GET', url=url)
+    value = await response.text()
+    value = json.loads(value)
+    return sum(value['data'])
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        response = await worker('bob', 3, session)
+        print('response', response)
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - start
+    print(f'executed in {elapsed:0.2f} seconds')
