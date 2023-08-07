a440d821bb5d63c009b638d14d5c5ae754d73ac9
Eirik Thue <MrPrecise@users.noreply.github.com>
2023-08-07 18:46:41 +0200

    Creating Bokeh Figure
    Creating bokeh figure before adding data do it

--- Projects/Bokeh-Visualization/firstFigure.py
+++ Projects/Bokeh-Visualization/firstFigure.py
@@ -7,8 +7,28 @@ from bokeh.plotting import figure, show
 output_file('output_file_test.html',
             title='Empty Bokeh Figure')
 
-# Set up a generic figure() object
-fig = figure()
+# Example figure
+fig = figure(background_fill_color='gray',
+             background_fill_alpha=0.5,
+             border_fill_color='blue',
+             border_fill_alpha=0.25,
+             height=300,
+             width=500,
+             x_axis_label='X Label',
+             x_axis_type='datetime',
+             x_axis_location='above',
+             x_range=('2018-01-01', '2018-06-30'),
+             y_axis_label='Y Label',
+             y_axis_type='linear',
+             y_axis_location='left',
+             y_range=(0, 100),
+             title='Example Figure',
+             title_location='right',
+             toolbar_location='below',
+             tools='save')
+
+# Remove the gridlines from the figure() object
+fig.grid.grid_line_color = 'Blue'
 
 # See what it looks like
 show(fig)
